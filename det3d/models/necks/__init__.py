168e114d8207367fc0a651f2127d528d30a52e29
yoon <ykw6644@gmail.com>
2023-12-11 09:09:35 +0000

    set for poolformer

---
+++ det3d/models/necks/__init__.py
@@ -0,0 +1,12 @@
+from .rpn import RPN
+from .rpn_transformer import RPN_transformer, RPN_transformer_deformable, RPN_transformer_multiframe, RPN_transformer_deformable_mtf
+from .rpn_transformer_multitask import RPN_transformer_multitask, RPN_transformer_deformable_multitask, RPN_poolformer_multitask
+
+__all__ = ["RPN",
+           "RPN_transformer",
+           "RPN_transformer_deformable",
+           "RPN_transformer_multiframe",
+           "RPN_transformer_deformable_mtf",
+           "RPN_transformer_multitask",
+           "RPN_transformer_deformable_multitask",
+           "RPN_poolformer_multitask"]
